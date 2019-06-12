33120d5765878826f2d0f7c50aa2c47fc46e745e
yaroslav-codefresh <yaroslav@codefresh.io>
2019-06-12 19:41:14 +0300

    add date

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 async function printLog(time) {
     for (let i = 0; i < 1000; i++) {
-        console.log('aaa'.repeat(100) + `_${time}_log_${i}`)
+        console.log('aaa'.repeat(100) + `_${time}_log_${i}_${new Date().toLocaleString()}`)
     }
 }
 
 async function printError(time) {
     for (let i = 0; i < 1000; i++) {
-        console.error('bbb'.repeat(100) + `_${time}_error_${i}`)
+        console.error('bbb'.repeat(100) + `_${time}_error_${i}_${new Date().toLocaleString()}`)
     }
 }
 
